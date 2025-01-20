ae8c65348a93a48233288de1c9742c95dac635e3
Julian Herrera <juliansantiago.rubianoherrera@gmail.com>
2025-01-20 14:54:48 -0500

    Card Desing and "See More" Button

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -2,16 +2,16 @@ import { useState, useEffect } from "react";
 import "./Section.css";
 
 function Section() {
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(14);
   const [championsInfo, setChampionsInfo] = useState([]);
-  const [mostrarBoton, setMostrarBoton]= useState(true)
-  const totalChampions =Object.keys(championsInfo).length
+  const [mostrarBoton, setMostrarBoton] = useState(true);
+  const totalChampions = Object.keys(championsInfo).length;
   // console.log(totalChampions)
- 
-  const verMas=()=>{
-    setCount(count+10)
-  }
-  
+
+  const verMas = () => {
+    setCount(count + 14);
+  };
+
   useEffect(() => {
     if (totalChampions > 0 && count >= totalChampions) {
       setMostrarBoton(false);
@@ -23,30 +23,36 @@ function Section() {
       `https://ddragon.leagueoflegends.com/cdn/15.1.1/data/en_US/champion.json`
     )
       .then((res) => res.json())
-      .then((champions) => {  
+      .then((champions) => {
         setChampionsInfo(champions.data);
-        // console.log(champions.data);
+        console.log(champions.data);
       });
   }, []);
   return (
     <>
-      <ul className="card">
-        {Object.values(championsInfo).slice(0,count).map((champion) => (
-          
-          <section key={champion.id}>
-            <img
-              className="image"
-              src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name}_0.jpg`}
-              alt={`Image of ${champion.name}`}
-            />
-            <h1 className="text">{champion.name}</h1>
-            <h2 className="text">{champion.title}</h2>
-            <h3 className="text">{champion.tags}</h3>
-          </section>
-        ))}
-      </ul>
+      <div className="card">
+        {Object.values(championsInfo)
+          .slice(0, count)
+          .map((champion) => (
+            <section className="cardChampion" key={champion.id}>
+              <img
+                className="image"
+                src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.name
+                  .replace(/\s+/g, "")
+                  .normalize("NFD")
+                  .replace(/[^a-zA-Z0-9]/g, '')
+                  .replace(/[\u0300-\u036f]/g, "")}_0.jpg`}
+                  
+                alt={`Image of ${champion.name}`}
+              />
+              <h1 className="text">{champion.name}</h1>
+              <h2 className="text">{champion.title}</h2>
+              <h3 className="text">{champion.tags}</h3>
+            </section>
+          ))}
+      </div>
+
       {mostrarBoton ? <button onClick={verMas}>Ver mas</button> : null}
-      
     </>
   );
 }
